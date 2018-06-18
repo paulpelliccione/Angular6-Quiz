396c197837218d739a987a3d79a8dc01c87a3841
topjobstar0605 <topjobstar@gmail.com>
2018-06-17 17:08:27 -0700

    Add question component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {QuestionComponent} from './question.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule
