41cd0530c08460add8b2ba1c414ed0064ab27d2f
topjobstar0605 <topjobstar@gmail.com>
2018-06-17 17:18:47 -0700

    Add Angular Material with Test button

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+
+
 import { AppComponent } from './app.component';
 import {QuestionComponent} from './question.component';
 
@@ -10,7 +14,10 @@ import {QuestionComponent} from './question.component';
     QuestionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/question.component.ts
+++ frontend/src/app/question.component.ts
@@ -2,7 +2,7 @@ import { Component} from '@angular/core';
 
 @Component ({
     selector: 'question',
-    template: 'this is our question' 
+    template: 'this is our question <button mat-button>Test</button>'
 })
 export class QuestionComponent {  
     
