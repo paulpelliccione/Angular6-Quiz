bbd11141e479a18cfc22bbaae481aaa458e47289
topjobstar0605 <topjobstar@gmail.com>
2018-06-18 20:21:13 -0700

    Add observable for action of selected question so selected question properites holds refrence to the question

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,14 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http"
+import { Subject } from 'rxjs';
 
 
 @Injectable() 
 export class ApiService {
 
+    private selectedQuestion = new Subject<any>();
+    questionSelected = this.selectedQuestion.asObservable();
+
+    
     constructor(private http: HttpClient) { }
 
     getQuestions(){
       return this.http.get('http://localhost:61142/api/questions');
+
         }
 
         postQuestion(question) {
@@ -16,5 +22,9 @@ export class ApiService {
                 console.log(res)
      })
     
+    }
+
+    selectQuestion(question) {
+        this.selectedQuestion.next(question)
     }
 }
