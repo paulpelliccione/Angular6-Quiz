c9b9921ef9cb7f399d10edf7b50a533c8627bec2
topjobstar0605 <topjobstar@gmail.com>
2018-06-20 00:25:17 -0700

    Convert Url string for apiz get questions to template literal to associate questions with their iD

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -13,8 +13,8 @@ export class ApiService {
 
     constructor(private http: HttpClient) {}
 
-    getQuestions(){
-        return this.http.get('http://localhost:61142/api/questions');
+    getQuestions(quizId){
+        return this.http.get(`http://localhost:61142/api/questions/${quizId}`);
     }
 
     getQuizzes(){
