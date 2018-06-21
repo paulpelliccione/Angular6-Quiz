dc47d0dd32c7fc0e556bf0a5ceb4879627697a84
topjobstar0605 <topjobstar@gmail.com>
2018-06-21 01:01:44 -0700

    Fix auth service route missing account path

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,11 +20,11 @@ import { NavComponent } from './nav.component';
 import { QuizComponent } from './quiz.component';
 import { QuizzesComponent } from './quizzes.component';
 import {RegisterComponent} from './register.component'
+import {LoginComponent} from './login.component'
 
 import { ApiService } from './api.service';
 import { AuthService } from './auth.service';
 import { AuthInterceptor } from './auth.interceptor';
-
 
 
 const routes = [
@@ -32,9 +32,8 @@ const routes = [
   {path: 'question', component: QuestionComponent},
   {path: 'question/:quizId', component: QuestionComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'quiz', component: QuizComponent},
-
-
 ]
 
 @NgModule({
@@ -46,7 +45,8 @@ const routes = [
     NavComponent,
     QuizComponent,
     QuizzesComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
 
 
   ],
--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { Subject} from 'rxjs'
+import { Router } from '@angular/router'
 
 @Injectable()
 export class AuthService {
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private router: Router) { }
 
-    register(credentials){
-     this.http.post<any>(`http://localhost:61142/api/account`, credentials).subscribe(res=> {
-        this.authenticate(res)
-     })
-    }
-    login(credentials){
-        this.http.post<any>(`http://localhost:61142/api/login`, credentials).subscribe(res=> {
+    register(credentials) {
+        this.http.post<any>(`http://localhost:61142/api/account`, credentials).subscribe(res => {
             this.authenticate(res)
         })
-       }
-       
-       authenticate(res) {
+    }
+    login(credentials) {
+        this.http.post<any>(`http://localhost:61142/api/account/login`, credentials).subscribe(res => {
+            this.authenticate(res)
+        })
+    }
+
+    authenticate(res) {
         localStorage.setItem('token', res)
-       }
+
+        this.router.navigate(['/'])
+    }
 
 }
