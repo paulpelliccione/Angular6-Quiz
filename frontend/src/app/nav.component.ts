0cc1460a9051da9a10c96ba8c81807f80e365dc7
topjobstar0605 <topjobstar@gmail.com>
2018-06-21 19:31:34 -0700

    Add authenticate function for register and logging in for nav component

--- frontend/src/app/nav.component.ts
+++ frontend/src/app/nav.component.ts
@@ -7,8 +7,8 @@ import {AuthService} from './auth.service'
     <mat-toolbar>
         <button mat-button routerLink="/">Quiz</button>
         <span style="flex: 1 1 auto;"></span>
-        <button mat-button routerLink="/register">Register</button>
-        <button mat-button routerLink="/login">Login</button>
+        <button *ngIf="!auth.isAuthenticated" mat-button routerLink="/register">Register</button>
+        <button *ngIf="!auth.isAuthenticated" mat-button routerLink="/login">Login</button>
         <button *ngIf="auth.isAuthenticated" mat-button (click)="auth.logout()">Logout</button>
     </mat-toolbar>
   `
